b4f08ed1587eb22a6324144b43a79cd7a09aa453
João Faro <joaobremgartner@gmail.com>
2016-10-03 15:15:14 -0300

    Ajuste nos testes da lógica de descontos

--- core/src/test/java/com/joaobremgartner/vendas/models/saidaLog/DescontoLogTest.java
+++ core/src/test/java/com/joaobremgartner/vendas/models/saidaLog/DescontoLogTest.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.joaobremgartner.vendas.calculos.CalculadoraDeDescontos;
 import com.joaobremgartner.vendas.enums.TPPessoa;
 import com.joaobremgartner.vendas.models.Cliente;
 import com.joaobremgartner.vendas.models.Orcamento;
@@ -11,7 +12,7 @@ import com.joaobremgartner.vendas.models.Produto;
 import com.joaobremgartner.vendas.models.builders.CriadorDeCliente;
 import com.joaobremgartner.vendas.models.builders.CriadorDeProduto;
 
-public class OrcamentoLogTest {
+public class DescontoLogTest {
 
 	public static void main(String[] args) {
 		// Forçando produto unitário...
@@ -20,23 +21,32 @@ public class OrcamentoLogTest {
 		Cliente cliente = CriadorDeCliente.novoCliente("João Faro", TPPessoa.FISICA).criar();
 		ArrayList<Produto> produtos = new ArrayList<>();
 		produtos.add(produto);
-		
-		Orcamento orcamento = new Orcamento(cliente, produtos, new BigDecimal(50.0));
-		
+
+		Orcamento orcamento = new Orcamento(cliente, produtos, new BigDecimal(504.0));
+
 		System.out.println(orcamento);
-		
+
 		// Criando uma lista de produtos no orçamento...
-		List<Produto> listaDeProdutos = CriadorDeProduto.novoProduto(2L, "Base MK", new BigDecimal(10.0), 
-				new BigDecimal(5), 2)
-				.mais(5)
-				.criarTodos();
+		List<Produto> listaDeProdutos = CriadorDeProduto
+				.novoProduto(2L, "Base MK", new BigDecimal(200.0), new BigDecimal(5), 2).mais(5).criarTodos();
 		// Realizando a soma dos produtos como valor total de orçamento
 		BigDecimal total = BigDecimal.valueOf(0.0);
-		for (Produto prd : listaDeProdutos) { 
+		for (Produto prd : listaDeProdutos) {
 			total = total.add(prd.getVlrVenda());
 		}
 		Orcamento orcamento2 = new Orcamento(cliente, listaDeProdutos, total);
+		CalculadoraDeDescontos calculadoraDeDescontos = new CalculadoraDeDescontos();
+		
+		// Calculando descontos
+		double desconto = calculadoraDeDescontos.calculaDesconto(orcamento2);
+		
 		// Imprimindo resultado
-		System.out.println(orcamento2);
+		System.out.println("Valor total: " + orcamento2);
+
+		System.out.println("Desconto: " + desconto);
+		
+		double totalComDesconto = orcamento2.getValorOrcamento().doubleValue() - desconto;
+		
+		System.out.println("Valor total com desconto: "+ totalComDesconto);
 	}
 }
