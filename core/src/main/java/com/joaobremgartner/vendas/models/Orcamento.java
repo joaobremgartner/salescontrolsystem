8dba8be038cb2946902859d70c2b8143de48b0e7
João Faro <joaobremgartner@gmail.com>
2016-10-06 12:38:43 -0300

    Acrescentando novo atributo - Tipo de pagamento

--- core/src/main/java/com/joaobremgartner/vendas/models/Orcamento.java
+++ core/src/main/java/com/joaobremgartner/vendas/models/Orcamento.java
@@ -3,6 +3,25 @@ package com.joaobremgartner.vendas.models;
 import java.math.BigDecimal;
 import java.util.List;
 
+import com.joaobremgartner.vendas.enums.CondicaoPagamento;
+
+/**
+ * <p>
+ * Classe responsável por representar a entidade {@code orçamento}. 
+ * </p>
+ * 
+ * @author Engº. João Paulo Bremgartner Faro - CREA: 27708D-PA
+ * 
+ * @see
+ * 		Cliente
+ * @see
+ * 		Produto
+ * @since
+ * 		1.8
+ * @version
+ * 		1.0.0
+ *
+ */
 public class Orcamento {
 
 	private Long id;
@@ -10,14 +29,16 @@ public class Orcamento {
 	private Cliente cliente;
 	private List<Produto> produtos;
 	private BigDecimal valorOrcamento;
+	private CondicaoPagamento condicao;
 
 	public Orcamento() {
 	}
 
-	public Orcamento(Cliente cliente, List<Produto> produtos, BigDecimal valor) {
+	public Orcamento(Cliente cliente, List<Produto> produtos, BigDecimal valor, CondicaoPagamento condicao) {
 		this.cliente = cliente;
 		this.produtos = produtos;
 		this.valorOrcamento = valor;
+		this.condicao = condicao;
 	}
 
 	public Cliente getCliente() {
@@ -26,6 +47,14 @@ public class Orcamento {
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+	
+	public CondicaoPagamento getCondicao() {
+		return condicao;
+	}
+
+	public void setCondicao(CondicaoPagamento condicao) {
+		this.condicao = condicao;
 	}
 
 	public List<Produto> getProdutos() {
