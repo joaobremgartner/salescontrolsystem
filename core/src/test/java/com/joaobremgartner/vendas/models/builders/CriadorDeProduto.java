98078a0bbfcb441bd9b720a76d2e8763fcc5a87d
João Faro <joaobremgartner@gmail.com>
2016-11-03 21:27:34 -0200

    refatorando builders

--- core/src/test/java/com/joaobremgartner/vendas/models/builders/CriadorDeProduto.java
+++ core/src/test/java/com/joaobremgartner/vendas/models/builders/CriadorDeProduto.java
@@ -9,23 +9,50 @@ import com.joaobremgartner.vendas.models.Fornecedor;
 import com.joaobremgartner.vendas.models.Produto;
 
 public class CriadorDeProduto {
-	
+
 	private List<Produto> produtos = new ArrayList<>();
-	
+
 	public CriadorDeProduto(Produto produto) {
 		this.produtos.add(produto);
 	}
-	
-	public static CriadorDeProduto novoProduto(Long id, String nome, BigDecimal valorVenda, BigDecimal valorCusto, int qtd) {
+
+	public static CriadorDeProduto novoProduto(Long id, String nome, BigDecimal valorVenda, BigDecimal valorCusto,
+			int qtd) {
 		Produto prd = criandoNovoProduto(id, nome, valorVenda, valorCusto, qtd);
-		
+
 		return new CriadorDeProduto(prd);
 	}
 
-	private static Produto criandoNovoProduto(Long id, String nome, BigDecimal valorVenda, BigDecimal valorCusto, int qtd) {
+	private static Produto criandoNovoProduto(Long id, String nome, BigDecimal valorVenda, BigDecimal valorCusto,
+			int qtd) {
 		Produto produto = new Produto();
+
+		populaProdutoComDados(produto, id, nome, valorVenda, valorCusto, qtd);
+
+		return produto;
+	}
+
+	public CriadorDeProduto mais(int qtd) {
+		Produto prdBase = this.produtos.get(0);
+		for (int i = 0; i < qtd; i++) {
+			this.produtos.add(criandoNovoProduto(prdBase.getId() + i, prdBase.getNmProduto(), prdBase.getVlrVenda(),
+					prdBase.getVlrCusto(), prdBase.getMaxEstoque() + i));
+		}
+		return this;
+	}
+
+	public Produto criar() {
+		return this.produtos.get(0);
+	}
+
+	public List<Produto> criarTodos() {
+		return this.produtos;
+	}
+
+	private static void populaProdutoComDados(Produto produto, Long id, String nome, BigDecimal valorVenda,
+			BigDecimal valorCusto, int qtd) {
 		ArrayList<Fornecedor> fornecedores = new ArrayList<>();
-		
+
 		produto.setId(id);
 		produto.setCodProduto("PRD-0012016");
 		produto.setNmProduto(nome);
@@ -35,31 +62,14 @@ public class CriadorDeProduto {
 		produto.setMaxEstoque(100);
 		produto.setEstoque(qtd);
 		// Adicionando categoria fixa de produtos (para testes)
-		// Caso haja alteração no valor da categoria, terá que mudar também na classe de testes 
+		// Caso haja alteração no valor da categoria, terá que mudar também na
+		// classe de testes
 		produto.setCategoria(CategoriaProduto.COSMETICOS);
-		
-		Fornecedor fornecedor = CriadorDeFornecedor.novoFornecedorPF(1L, "João Faro", "123.456.789-00", "1234567").criar();
+
+		Fornecedor fornecedor = CriadorDeFornecedor.novoFornecedorPF(1L, "João Faro", "123.456.789-00", "1234567")
+				.criar();
 		fornecedores.add(fornecedor);
-		
+
 		produto.setFornecedores(fornecedores);
-		
-		return produto;
-	}
-	
-	public CriadorDeProduto mais(int qtd) {
-		Produto prdBase = this.produtos.get(0);
-		for (int i = 0; i < qtd; i++) {
-			this.produtos.add(criandoNovoProduto(prdBase.getId() + i, prdBase.getNmProduto(), prdBase.getVlrVenda(), prdBase.getVlrCusto(), 
-					prdBase.getMaxEstoque() + i));
-		}
-		return this;
-	}
-	
-	public Produto criar() {
-		return this.produtos.get(0);
-	}
-	
-	public List<Produto> criarTodos() {
-		return this.produtos;
 	}
 }
